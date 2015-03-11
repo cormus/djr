d3a07b4ee05505ee1e09d501a76d05da50740ff8
Alex <alexprodutor@hotmail.com>
2015-03-11 15:35:36 -0300

    Inicio do projeto

---
+++ www/controllers/angular-controller.js
@@ -0,0 +1,33 @@
+angularConfig.controller('TelaCtrl', function($rootScope, $location)
+{
+	window.setTimeout(function(){
+
+		var gruposList		   = [];
+
+		function loadGrupo()
+		{
+			//realiza uma busca no banco de dados
+			function queryDBGrupo(tx) 
+			{
+				tx.executeSql("SELECT * FROM framwork_teste", [], querySuccessGrupo, errorCB);
+			}
+			function querySuccessGrupo(tx, results) 
+			{
+				gruposList = results.rows;
+			}
+			db.transaction(queryDBGrupo, errorCB);
+		}
+});
+
+angularConfig.controller('ContatoCtrl', function($rootScope, $location)
+{
+    //fecha o snap
+    appClass.snap.close();
+    //coloca uma classe se ativado no link do menu
+    $rootScope.activetab = $location.path();
+});  
+
+angularConfig.controller('HomeCtrl', function($rootScope, $location)
+{
+	$location.path('tela');    
+}); 
