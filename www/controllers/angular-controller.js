63164c8b729c239ba20160a42596f57203be7609
Alex <alexprodutor@hotmail.com>
2015-03-19 10:24:14 -0300

    Atualização dos controle

--- www/controllers/angular-controller.js
+++ www/controllers/angular-controller.js
@@ -17,6 +17,19 @@ angularConfig.controller('CadastroFormularioCtrl', function($rootScope, $locatio
     //coloca uma classe se ativado no link do menu
     $rootScope.activetab = $location.path();
 	
+	//cadastro-nome
+	//cadastro-endereco
+	//cadastro-cep
+	//cadastro-cidade
+	//cadastro-bairro
+	//cadastro-estado
+	//cadastro-nascimento
+	//cadastro-email
+	//cadastro-telefone
+	//cadastro-celular
+	//cadastro-whatsapp
+	//cadastro-receer-informacao
+	
 	$rootScope.historicoFormulario = function()
 	{
 		$location.path('historico_formulario');
@@ -30,6 +43,19 @@ angularConfig.controller('HistoricoFormularioCtrl', function($rootScope, $locati
     $rootScope.activetab = $location.path();
 	
 	
+	//historico-medicamento
+	//historico-medicamento-tipo
+	//historico-alergia
+	//historico-alergia-tipo
+	//historico-cardiaco
+	//historico-cardiaco-tipo
+	//historico-hormonais
+	//historico-hormonais-tipo
+	//historico-tratamento
+	//historico-hipertensao
+	//historico-diabetico
+	//historico-clinico
+	
 	$rootScope.habitosFormulario = function()
 	{
 		$location.path('habitos_formulario');
@@ -42,6 +68,13 @@ angularConfig.controller('HabitosFormularioCtrl', function($rootScope, $location
     appClass.snap.close();
     //coloca uma classe se ativado no link do menu
     $rootScope.activetab = $location.path();
+	
+	//habitos-filtro
+	//habitos-cosmeticos
+	//habitos-sol
+	//habitos-tabagismo
+	//habitos-tratamento
+	//habitos-tratamento-quais
 	
 	$rootScope.tratamentosFormulario = function()
 	{
